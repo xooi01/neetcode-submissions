967521d2009d9674ca73979d1167359c9e303868
xooi01 <59626635+xooi01@users.noreply.github.com>
2026-09-16 14:37:05 -0400

    Add: two-integer-sum-ii - submission-4

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-4.py
@@ -0,0 +1,23 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        # two pointer: look at left and right
+        # the list is ordered from smallest to largest
+        # add the l + r
+        #       if l + r > target, go to next right idx
+        #       if l + r < targer, go to next left idx
+        if len(numbers) == 0:
+            return []
+        l_idx = 0
+        r_idx = len(numbers) - 1
+        for i in range(len(numbers)):
+            addLR = numbers[l_idx] + numbers[r_idx]
+            if addLR > target:
+                r_idx -= 1
+            elif addLR < target:
+                l_idx += 1
+            elif addLR == target:
+                return [l_idx+1, r_idx+1]
+
+# 10:02, 1-indexing!!
+# Time Complexity: O(N)
+# Space Complexity: O(1)
