5533eaaf8c2adc651ff9578acfa64e4d67dbac50
xooi01 <59626635+xooi01@users.noreply.github.com>
2026-09-16 22:15:50 -0400

    Add: buy-and-sell-crypto - submission-9

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-9.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        # if prices == sorted(prices, reverse=True):
+        #     return 0
+        # if prices == sorted(prices):
+        #     return max(prices) - min(prices)
+
+        maxSell = 0
+        for i in range(1, len(prices)):
+            if prices[i] - min(prices[:i]) > maxSell:
+                maxSell = prices[i] - min(prices[:i])
+        return maxSell
+# 21:05, needed the hints
+# Time complexity: O(N2)
+# Space complextiy: O(N)
+        
